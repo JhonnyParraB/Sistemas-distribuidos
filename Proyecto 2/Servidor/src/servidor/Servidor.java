e0414a0717f91527ec1a4e6706af5a53057d399f
Juan Sebastian Triana Perez <green36363@gmail.com>
2019-05-17 01:55:05 -0500

    Leer productos en servidor

--- Proyecto 2/Servidor/src/servidor/Servidor.java
+++ Proyecto 2/Servidor/src/servidor/Servidor.java
@@ -5,6 +5,14 @@
  */
 package servidor;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  *
  * @author green
@@ -16,6 +24,45 @@ public class Servidor {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Map<String, Integer> productos = leerProductos();
+
     }
-    
+
+    public static Map<String, Integer> leerProductos() {
+        Map<String, Integer> productos = new HashMap<String, Integer>();
+        File archivo = null;
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            // Apertura del fichero y creacion de BufferedReader para poder
+            // hacer una lectura comoda (disponer del metodo readLine()).
+            archivo = new File("Productos.txt");
+            fr = new FileReader(archivo);
+            br = new BufferedReader(fr);
+
+            // Lectura del fichero
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String lines[] = linea.split(" ");
+                productos.put(lines[0], Integer.parseInt(lines[1]));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // En el finally cerramos el fichero, para asegurarnos
+            // que se cierra tanto si todo va bien como si salta 
+            // una excepcion.
+            try {
+                if (null != fr) {
+                    fr.close();
+                }
+                return productos;
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        return productos;
+    }
+
 }
