80cc4c5397af2a4d9c5837dd65f17f4e890c7df0
Jhonny <jhonny_parra@javeriana.edu.co>
2019-05-17 02:26:17 -0500

    Menú usuario completo

--- Proyecto 2/Cliente/src/cliente/Cliente.java
+++ Proyecto 2/Cliente/src/cliente/Cliente.java
@@ -25,6 +25,16 @@ public class Cliente {
 
     private static RMIInterfaceCoordinador look_up_coordinador;
     private static Scanner in = new Scanner (System.in);
+    
+    private static final int REGISTRO = 1;
+    private static final int INICIAR_COMPRA = 2;
+    private static final int CONSULTAR_SALDO = 3;
+    private static final int INGRESAR_DINERO = 4;
+    private static final int SALIR = 5;
+    
+    
+    private static final long SALDO_INICIAL = 5000;
+    
 
     /**
      * @param args the command line arguments
@@ -38,23 +48,60 @@ public class Cliente {
         look_up_coordinador = (RMIInterfaceCoordinador) registry.lookup("//127.0.0.1/Coordinador");
         
         
-        registrar();
-        
-        
-        
-        
+              
+        boolean mostrarOpciones = true;
+        int opcion  = -1;
+        while (opcion != 5){
+            if (mostrarOpciones){
+                System.out.println ("<<< Menú principal >>>");
+                System.out.println("1. Registrarse");
+                System.out.println("2. Iniciar compra");
+                System.out.println("3. Consultar saldo");
+                System.out.println("4. Ingresar dinero");
+                System.out.println("5. Salir");
+            }
+            
+            System.out.print("Opción: ");
+            opcion = in.nextInt();
+            mostrarOpciones = true;
+            
+            switch (opcion){
+                case REGISTRO:
+                    registrar();
+                    break;
+                case INICIAR_COMPRA:
+                    break;
+                case CONSULTAR_SALDO:
+                    break;
+                case INGRESAR_DINERO:
+                    break;
+                case SALIR:
+                    break;
+                default:
+                    System.out.println ("Opcion incorrecta, inténtelo nuevamente.");
+                    mostrarOpciones = false;
+            }
+        }
+                   
     }
     
     private static void registrar() throws RemoteException{
         String contrasena;
-        String nombre_usuario;
-        
+        String nombre_usuario;        
         
         System.out.println("-------------Registro-------------\n");
         System.out.println("Ingrese el nombre de usuario:");
-        nombre_usuario = in.nextLine();
+        nombre_usuario = in.next();
         System.out.println("Ingrese la contraseña:");
-        contrasena = in.nextLine();
-        System.out.println ("Este es su número de tarjeta: "+ look_up_coordinador.registrarUsuarioBanco(nombre_usuario, contrasena));
+        contrasena = in.next();
+        
+        long numeroTarjeta = look_up_coordinador.registrarUsuarioBanco(nombre_usuario, contrasena);
+        System.out.println ("Bienvenido, su nombre de usuario y contraseña ha sido registrado.");
+        System.out.println ("El número de tarjeta es: "+ numeroTarjeta);
+        
+        System.out.println ("Recuerde que inicia con un saldo inicial de "+ SALDO_INICIAL);
+        
     }
+    
+    
 }
